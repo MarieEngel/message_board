3e8938f568288bce102d9dd3a1cc4fa7aa6b58bb
anna.kowalczyk <kowalczyk.anna01@gmail.com>
2022-03-30 18:26:57 +0200

    basic register page

--- user/urls.py
+++ user/urls.py
@@ -7,6 +7,7 @@ app_name = 'user'
 
 urlpatterns = [
     path("profile/", user_views.user_profile, name="user-profile"),
+    path("register/", user_views.register, name="register"),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.urls import reverse
+
+from . import models
+from .forms import UserRegisterForm
 
 
 # Create your views here.
+@transaction.atomic
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            user = User.objects.get(username=username)
+            profile = models.Profile.objects.create(user=user)
+            profile.save()
+
+            messages.success(request, f'Account created for {username}!', fail_silently=True)  # we can also use add_message() method instead of the shortcuts
+            return redirect(reverse('user:login'))
+    else:
+        form = UserRegisterForm()
+    return render(request, "user/register.html", {'form': form})
+
 @login_required
 def user_profile(request):
     return render(request, "user/profile.html")
