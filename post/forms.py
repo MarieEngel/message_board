b77d7bd4556618f821699b2f449be12319653fd4
Marie Engel <marie.engel@deliveryhero.com>
2022-03-31 09:49:11 +0200

    Add basic functions for posting

---
+++ post/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms import HiddenInput, ModelForm
+
+from .models import Post
+
+class AddPostForm(ModelForm):
+    class Meta:
+        model = Post
+        exclude = ["user", "modified", "created", "solved"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "photo": forms.FileInput(attrs={"class": "form-control"}),
+            "body": forms.Textarea(attrs={"class": "form-control"}),            
+        }
