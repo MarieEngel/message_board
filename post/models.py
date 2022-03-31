0f5de48045b83c9bd167984a59849e06645652c6
Marie Engel <marie.engel@deliveryhero.com>
2022-03-31 18:40:59 +0200

    Black lint

--- post/models.py
+++ post/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 #     city = models.CharField(max_length=100)
 #     postal_code = models.CharField(max_length=4)
 
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     photo = models.ImageField(null=True, blank=True, upload_to="images/")
@@ -18,8 +19,3 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.user}"
-
-
-
-
-
