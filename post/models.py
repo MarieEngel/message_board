c0c1cf843f097d435523d158cc84b7efb713184a
Anya Huang <huangpailu1981@gmail.com>
2022-03-31 19:55:32 +0200

    fix post.html and add date_modified in model

--- post/models.py
+++ post/models.py
@@ -26,6 +26,7 @@ class Comment(models.Model):
     name = models.CharField(max_length=100)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-    
+    date_modified = models.DateTimeField(auto_now=True)
+   
     def __str__(self) -> str:
         return '%s - %s' % (self.post.title, self.name)
