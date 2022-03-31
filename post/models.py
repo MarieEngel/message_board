6349e1c23e456ea33a0441acda7a4e1ecd0b625f
Anya Huang <huangpailu1981@gmail.com>
2022-03-31 14:49:49 +0200

    add comment to post app

--- post/models.py
+++ post/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments",on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return '%s - %s' % (self.post.title, self.name)
