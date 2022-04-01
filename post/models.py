828d87f361e8ad79fe3e0b40befbc49624da56ad
Anya Huang <huangpailu1981@gmail.com>
2022-04-01 11:37:11 +0200

    changed name to user in model

--- post/models.py
+++ post/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
