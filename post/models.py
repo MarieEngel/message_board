729d1dd35f5efb7cb7d5feaf6bb65317b15b0d31
Anya Huang <huangpailu1981@gmail.com>
2022-04-02 16:07:12 +0200

    changed model and now comment works

--- post/models.py
+++ post/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
--- post/urls.py
+++ post/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("add/", post_views.add_post, name="add-post"),
     path("<int:id>/delete/", post_views.delete_post, name="delete-post"),
     path("<int:id>/update/", post_views.update_post, name="update-post"),
-    path("<int:id>/comment/",AddCommentView.as_view(), name="add_comment"),
+    path("<int:pk>/comment/",AddCommentView.as_view(), name="add_comment"),
     #path("<int:id>/comment/", post_views.add_comment, name="add-comment"),
 ]
