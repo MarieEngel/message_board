1679ee9c0128b7d764beb04ae9714061ca3ae0d1
Marie Engel <marie.engel@deliveryhero.com>
2022-04-04 18:06:57 +0200

    Add AWS region

--- message_board/settings.py
+++ message_board/settings.py
@@ -168,9 +168,9 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = "samso-bucket"
 AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_REGION_NAME = "eu-central-1"
 
 django_heroku.settings(locals())
-
 
 
 try:
