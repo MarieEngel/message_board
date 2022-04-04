3e9e587d63c55d0c302114de251e12d168ea385f
Marie Engel <marie.engel@deliveryhero.com>
2022-04-04 17:57:38 +0200

    Add bucket

--- message_board/settings.py
+++ message_board/settings.py
@@ -161,13 +161,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SECURE_SSL_REDIRECT  = True
-
 AWS_S3_FILE_OVERWRITE = False
 
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = "samso-bucket"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 django_heroku.settings(locals())
 
