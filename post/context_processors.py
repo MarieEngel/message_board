715d7be95edec44ab106dc74a86f961eebfd81fe
Marie Engel <marie.engel@deliveryhero.com>
2022-04-05 14:23:08 +0200

    Add search_form to context_processor

--- post/context_processors.py
+++ post/context_processors.py
@@ -1,5 +1,11 @@
 from .models import Category
+from .forms import SearchForm
 
 def all_categories(request):
     categories = Category.objects.all()
     return {'all_categories': categories}
+
+def forms_processor(request):
+    search_form = SearchForm()
+    context = {'name_form': name_form}
+    return context
