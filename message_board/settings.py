7fe91f8174afbd2ff4823962ecd3ab9dd712484d
Marie Engel <marie.engel@deliveryhero.com>
2022-04-07 10:44:29 +0200

    Add postgres as default

--- message_board/settings.py
+++ message_board/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'message_board.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'message_board',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
