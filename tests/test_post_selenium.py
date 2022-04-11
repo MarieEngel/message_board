6caf81556c671b14ac48cf9fe319bc17758ec235
Marie Engel <marie.engel@deliveryhero.com>
2022-04-11 09:48:02 +0200

    WIP add selenium test for delete post

--- tests/test_post_selenium.py
+++ tests/test_post_selenium.py
@@ -99,6 +99,59 @@ class SeleniumTestPost(StaticLiveServerTestCase):
         body = self.driver.find_element_by_id("post-body").text
         # self.assertIsNotNone(body)
         print(body)
-        self.assertContains(body, "My cat did not come home.")
+        self.assertEqual(body, "My cat did not come home.")
+
+    def test_delete_post(self):
+        """Tests deleting a post."""
+        self.driver.get(f"{self.live_server_url}/user/login/")
+        username_field = self.driver.find_element_by_name("username")
+        username_field.send_keys("testuser1")
+        password_field = self.driver.find_element_by_name("password")
+        password_field.send_keys("password")
+        login_button = self.driver.find_element_by_xpath("//button[@class='btn btn-submit-custom']")
+        login_button.click()
+        add_post_button = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(
+                (
+                    By.CSS_SELECTOR,
+                    "#navbarSupportedContent > ul > li:nth-child(2) > a",
+                )
+            )
+        )
+        add_post_button.click()
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, "id_title"))
+        )
+        post_title = self.driver.find_element_by_css_selector("#id_title")
+        post_title.send_keys('Missing dog')
+        post_body = self.driver.find_element_by_name('body')
+        post_body.send_keys('My dog did not come home.')
+
+        select = Select(self.driver.find_element_by_name("category"))
+        select.select_by_index(1)
+
+        # WebDriverWait(self.driver, 30).until(
+        #     EC.element_to_be_clickable(
+        #         (
+        #             By.XPATH,
+        #             "//button[@class='btn btn-submit-custom']",
+        #         )
+        #     )
+        # )
+        sleep(5)
+        submit_button = self.driver.find_element_by_xpath("//button[@class='btn btn-submit-custom']")
+        submit_button.click()
+        # WebDriverWait(self.driver, 10).until(
+        #     EC.presence_of_element_located((By.ID, "id_title"))
+        # )
+        sleep(5)
+        title = self.driver.find_element_by_id("post-title")
+        title.click()
+
+        delete_button = self.driver.find_element_by_xpath("//button[@class='btn btn-danger-custom btn-sm']")
+
+        # self.assertIsNotNone(body)
+        # print(body)
+        # self.assertEqual(body, "My dog did not come home.")
 
         
