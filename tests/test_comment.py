7b2576fddcef70bf55f15e1c1f71a584b42ac3fd
anna.kowalczyk <kowalczyk.anna01@gmail.com>
2022-04-12 19:13:35 +0200

    unittests for comments

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -24,3 +24,79 @@ class CommentTestCase(TestCase):
         self.assertContains(response, "This is test comment")
         response = self.client.get("/post/1/")
         self.assertContains(response, "This is test comment")
+
+    def test_update_comment(self):
+        self.client.login(username="anna", password="password")
+        response = self.client.get("/post/2/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "I have lost my cat! Please help")
+        self.assertNotContains(response, "This comment has been edited")
+        response = self.client.post(
+            "/post/2/comment/2/update/",
+            {"body": "This comment has been edited", "_save": "SAVE"},
+        )
+        self.assertRedirects(response, "/post/2/#comment-2")
+        response = self.client.get("/post/2/")
+        self.assertContains(response, "This comment has been edited")
+
+    def test_delete_comment(self):
+        self.client.login(username="marie", password="password")
+        response = self.client.get("/post/2/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "I have lost my cat! Please help")
+        self.assertContains(response, "Another short comment")
+        response = self.client.post(
+            "/post/2/comment/4/delete/",
+            {"_save": "SAVE"},
+        )
+        self.assertRedirects(response, "/post/2/")
+        response = self.client.get("/post/2/")
+        self.assertContains(response, "I have lost my cat! Please help")
+        self.assertNotContains(response, "Another short comment")
+
+    def test_delete_comment_unauthorised(self):
+        self.client.login(username="anna", password="password")
+        response = self.client.get("/post/2/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "I have lost my cat! Please help")
+        self.assertContains(response, "Another short comment")
+        response = self.client.post(
+            "/post/2/comment/4/delete/",
+            {"_save": "SAVE"},
+        )
+        self.assertEqual(response.status_code, 403)
+        response = self.client.get("/post/2/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "I have lost my cat! Please help")
+        self.assertContains(response, "Another short comment")
+
+    def test_update_comment_unauthorised(self):
+        self.client.login(username="marie", password="password")
+        response = self.client.get("/post/2/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "I have lost my cat! Please help")
+        self.assertNotContains(response, "This comment has been edited")
+        response = self.client.post(
+            "/post/2/comment/2/update/",
+            {"body": "This comment has been edited", "_save": "SAVE"},
+        )
+        self.assertEqual(response.status_code, 403)
+        response = self.client.get("/post/2/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "I have lost my cat! Please help")
+        self.assertNotContains(response, "This comment has been edited")
+
+    def test_add_comment_anonimous_user(self):
+        response = self.client.get("/post/1/comment/")
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/user/login/?next=/post/1/comment/")
+
+    def test_update_comment_anonimous_user(self):
+        response = self.client.get("/post/2/comment/2/update/")
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/user/login/?next=/post/2/comment/2/update/")
+
+    def test_delete_comment_anonimous_user(self):
+        response = self.client.get("/post/2/comment/4/delete/")
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/user/login/?next=/post/2/comment/4/delete/")
