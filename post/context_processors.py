d63f01f9e23814cfadec912c1c7f8e9357955354
anna.kowalczyk <kowalczyk.anna01@gmail.com>
2022-04-12 09:47:40 +0200

    format with black

--- post/context_processors.py
+++ post/context_processors.py
@@ -4,8 +4,9 @@ from .forms import SearchForm
 
 def forms_processor(request):
     search_form = SearchForm()
-    return {'search_form': search_form}
+    return {"search_form": search_form}
+
 
 def all_categories(request):
     categories = Category.objects.all()
-    return {'all_categories': categories}
+    return {"all_categories": categories}
--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views as post_views
-from post.views import AddCommentView 
+from post.views import AddCommentView
+
 app_name = "post"
 
 urlpatterns = [
@@ -8,8 +9,16 @@ urlpatterns = [
     path("add/", post_views.add_post, name="add-post"),
     path("<int:id>/delete/", post_views.delete_post, name="delete-post"),
     path("<int:id>/update/", post_views.update_post, name="update-post"),
-    path("<int:pk>/comment/",AddCommentView.as_view(), name="add_comment"),
+    path("<int:pk>/comment/", AddCommentView.as_view(), name="add_comment"),
     path("search/", post_views.search, name="search"),
-    path("<int:post_id>/comment/<int:pk>/delete/", post_views.DeleteCommentView.as_view(), name="delete-comment"),
-    path("<int:post_id>/comment/<int:pk>/update/", post_views.UpdateCommentView.as_view(), name="update-comment"),
+    path(
+        "<int:post_id>/comment/<int:pk>/delete/",
+        post_views.DeleteCommentView.as_view(),
+        name="delete-comment",
+    ),
+    path(
+        "<int:post_id>/comment/<int:pk>/update/",
+        post_views.UpdateCommentView.as_view(),
+        name="update-comment",
+    ),
 ]
