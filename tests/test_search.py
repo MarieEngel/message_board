703ce8d52ae660d43d8481e36ef36603b6d6b3de
Marie Engel <marie.engel@deliveryhero.com>
2022-04-13 14:01:20 +0200

    Add test for logged in user

--- tests/test_search.py
+++ tests/test_search.py
@@ -15,3 +15,14 @@ class TestSearch(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Login")
         self.assertContains(response, "Register")
+
+    def test_search(self):
+        """Tests that logged in users can search."""
+        self.client.login(username="marie", password="password")
+        response = self.client.get("/post/search?query=cow&categories=All", follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "My lovely cow is missing")
+        self.assertContains(response, "My cow is lost again")
+        self.assertNotContains(response, "I have lost my cat")
+        self.assertNotContains(response, "Silly post")
+        self.assertNotContains(response, "Demo")
