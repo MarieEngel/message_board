77bd8b91f734f60dadf40899d3442d7691d5214d
Marie Engel <marie.engel@deliveryhero.com>
2022-04-13 13:32:03 +0200

    Add anonymous search

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,19 +1,17 @@
-# from django.test import TestCase
-# from django.contrib.auth.models import User
+from django.test import TestCase
 
 
-# class TestSearch(TestCase):
-#     def setUp(self):
-#         self.user = User.objects.create_superuser(
-#             username="testuser", password="password"
-#         )
+class TestSearch(TestCase):
+    fixtures = [
+        "user_test.json",
+        "category_test.json",
+        "post_test.json",
+        "comment_test.json",
+    ]
 
-#     def tearDown(self):
-#         self.user.delete()
-
-#     def test_search_logged_out(self):
-#         """Tests that logged out users can't search."""
-#         response = self.client.get("/", follow=True)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertContains(response, "Login")
-#         self.assertContains(response, "Register")
+    def test_search_anonymous_user(self):
+        """Tests that logged out users can't search."""
+        response = self.client.get("/post/search?query=das", follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Login")
+        self.assertContains(response, "Register")
