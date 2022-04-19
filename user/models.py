85dfcce881d06d96eade1dfadbc8da874989512a
anna.kowalczyk <kowalczyk.anna01@gmail.com>
2022-04-19 15:59:14 +0200

    update_profile

--- user/models.py
+++ user/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def validate_postcode(value):
+    if value != "8305":
+        raise ValidationError("Only people from island Samsø can register")
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(
-        default="profile_pics/default.jpg", upload_to="profile_pics"
+        default="profile_pics/default.jpg", upload_to="profile_pics", blank=True
     )
     phone = models.CharField(max_length=20, null=True, blank=True)
     latitude = models.DecimalField(
@@ -14,7 +20,7 @@ class Profile(models.Model):
     longitude = models.DecimalField(
         max_digits=22, decimal_places=16, blank=True, null=True
     )
-    postcode = models.CharField(max_length=8)
+    postcode = models.CharField(max_length=8, validators=[validate_postcode])
     city = models.CharField(max_length=50)
     street = models.CharField(max_length=100, null=True, blank=True)
     street_number = models.PositiveIntegerField(null=True, blank=True)
