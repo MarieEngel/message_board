28d446957bab6ce13abfc759a3766e79ee7d4ca9
anna.kowalczyk <kowalczyk.anna01@gmail.com>
2022-04-19 15:44:50 +0200

    update_profile

--- user/models.py
+++ user/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from PIL import Image
-from django.forms import CharField
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
