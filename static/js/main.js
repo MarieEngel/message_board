1bea7d3a61098819a99ee79e146a68fe9ddded0b
SyncopatedPandemonium <kowalczyk.anna01@gmail.com>
2022-04-19 09:46:46 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -6,34 +6,24 @@ const getLocation = () => {
     const city = document.querySelector('.city');
 
     const success = (position) => {
-        // console.log(position)
 
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
 
         const API_KEY = 'AIzaSyBPR88unXRLKLEz3pK3soTyZpdQYI4hj6E'
-        // const latitude = 52.5432870179525400
-        // const longitude = 13.4146936213831380
-        // const latitude = 55.820032
-        // const longitude = 10.634549
 
         const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${API_KEY}`
         fetch(geocodingApiUrl)
-        .then(res => res.json())
+        .then(response => response.json())
         .then(data => {
-            console.log(data.results[0].address_components)
-            console.log(latitude)
             lat.value = latitude
-            console.log(longitude)
             lon.value = longitude
             let parts = data.results[0].address_components;
             parts.forEach(part => {
                 if (part.types.includes("postal_code")) {
-                    console.log(part.long_name)
                     postcode.value = part.long_name
                 }
                 if (part.types.includes("locality")) {
-                    console.log(part.long_name)
                     city.value = part.long_name
                 }
             });
