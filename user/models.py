a0c4d582f46159323e7fa45e35568778cd14d16b
anna.kowalczyk <kowalczyk.anna01@gmail.com>
2022-04-19 15:35:46 +0200

    update_profile

--- user/models.py
+++ user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from PIL import Image
+from django.forms import CharField
 
 
 class Profile(models.Model):
@@ -9,6 +10,7 @@ class Profile(models.Model):
     image = models.ImageField(
         default="profile_pics/default.jpg", upload_to="profile_pics"
     )
+    phone = models.CharField(max_length=20, null=True, blank=True)
     latitude = models.DecimalField(
         max_digits=22, decimal_places=16, blank=True, null=True
     )
